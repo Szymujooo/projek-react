13011f86b013c8be8368dfad39e4d1a68435ea95
Szymujooo <Sz.p505@wp.pl>
2024-09-17 11:17:45 +0200

    Pierwsze zajęcia

---
+++ src/components/InfoBox/InfoBox.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export const InfoBox = () =>{
+
+    const name= "Paweł"
+
+    return <p>Nazywam się {name}</p>
+}
+
+export default InfoBox;
---
+++ src/components/List/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const List = () =>{
+    const list = [1,2,3,4,5,6]
+
+    const changeList = (list) =>{
+        const tmp= list.map((x) => x+2)
+
+        return tmp
+    }
+    return <div>{list.map((x)=> {return x +", "})} {changeList(list).map((x)=> {return x + ", "})}</div>
+}
+
+export default List
